0c17e3bac765c318594ce1617325e6c694d9f215
Abagail Hudson <abagailanne@gmail.com>
2019-05-03 19:27:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,17 @@
 
 def countdown (x)
 while x > 0
-  if x == 0
-    puts "HAPPY NEW YEAR"
-  else
-    puts "#{x} SECOND(S)!"
-  end
   x -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  while x > 0
+      puts "#{x} SECOND(S)!"
+      x -= 1
+      sleep(1)
+    end
+  
+     "HAPPY NEW YEAR!"
 end
