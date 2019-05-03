dafe67f375fbd771b6b072240c0657527d3bc33f
Abagail Hudson <abagailanne@gmail.com>
2019-05-03 19:20:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
 def countdown
+  x = 100
+while x > 0
+  if x == 0
+    puts "HAPPY NEW YEAR"
+  elseif x == 1
+    puts "#{x} SECOND"
+  else 
+    puts "#{x} SECONDS"
+  end
+  x -= 1
+end
 end
