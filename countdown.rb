11d1c896c676172e0e15e51c42fa0eef43602362
Abagail Hudson <abagailanne@gmail.com>
2019-05-03 19:27:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 #write your code here
 
 def countdown (x)
-while x > 0
+while x!= 0
   x -= 1
 end
 "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(x)
-  while x > 0
+  while x != 0
       puts "#{x} SECOND(S)!"
       x -= 1
       sleep(1)
